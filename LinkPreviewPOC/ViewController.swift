d4044f4746ea3f7252a0e475179e9d88a918119b
Suraj Mirajkar <mirajkarsuraj@gmail.com>
2021-07-02 20:30:27 +0530

    Error handling

--- LinkPreviewPOC/ViewController.swift
+++ LinkPreviewPOC/ViewController.swift
@@ -18,18 +18,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        //self.view.addSubview(linkView)
-        //linkView.isHidden = true
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        //linkView.frame = CGRect(x: 0, y: 350, width: self.view.frame.size.width-100, height: 100)
     }
     
     
     @IBAction func getUrlPreview(_ sender: Any) {
-        if let urlStr = urlTextField.text, urlStr.isValidURL {
+        if let urlStr = urlTextField.text {
             var holderView = UIView(frame:CGRect.zero)
             holderView.frame = CGRect(x: 0, y: 350, width: self.view.frame.size.width, height: 100)
             holderView = self.view.addURLPreview(urlString: urlStr, frames: holderView.frame)
@@ -65,19 +62,22 @@ extension UIView {
         linkView.frame = .zero
         let metadataProvider = LPMetadataProvider()
         guard !urlStr.isEmpty, urlStr.isValidURL else {
-            print("Invalid URL inserted in TextField")
+            //print("Invalid URL inserted in TextField")
+            let alert = UIAlertController(title: "Error", message: "Invalid link entered in text box.", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Click", style: .default, handler: nil))
+            self.window?.rootViewController?.present(alert, animated: true, completion: {})
             return holderView
         }
         holderView.frame = frames
         linkView.frame = holderView.bounds
         holderView.addSubview(linkView)
-        if !urlStr.contains("http") || !urlStr.contains("https") {
+        if !urlStr.lowercased().contains("http") && !urlStr.lowercased().contains("https") {
             urlStr = "https://" + urlStr
         }
         let url = URL(string: urlStr)! //"https://developer.apple.com/documentation/linkpresentation/lplinkmetadata"
         metadataProvider.startFetchingMetadata(for: url) { metadata, error in
             if error != nil {
-                print("Error occurred while fetching metadata from url - \(String(describing: error?.localizedDescription))")
+                self.errorHandling(linkView: linkView, url: url, error: error!)
                 return
             }
             guard let metadataOfUrl: LPLinkMetadata = metadata else {
@@ -95,5 +95,16 @@ extension UIView {
             }
         }
         return holderView
+    }
+    
+    func errorHandling(linkView: LPLinkView, url: URL, error: Error?) {
+        let errorHandlingMetadata = LPLinkMetadata()
+        errorHandlingMetadata.title = "Unable to fetch meta data"
+        errorHandlingMetadata.url = url
+        errorHandlingMetadata.iconProvider = NSItemProvider.init(contentsOf: URL(fileURLWithPath: Bundle.main.path(forResource: "error", ofType: "png")!))
+        linkView.metadata = errorHandlingMetadata
+        if let currentError = error {
+            print("Error occurred while fetching metadata from url - \(String(describing: currentError.localizedDescription))")
+        }
     }
 }
